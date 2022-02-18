6f0db614a1d7c88c452cdc095cc1e598077421d0
estebanhernandezr <38502029+estebanhernandezr@users.noreply.github.com>
2022-02-18 10:12:00 -0500

    Add files via upload

--- Taller_1/Taller_1.js
+++ Taller_1/Taller_1.js
@@ -11,42 +11,87 @@ var nueve = document.getElementById("nueve_b");
 
 var barra = document.getElementById("barra_numerica");
 
+var mas = document.getElementById("mas_b");
+var menos = document.getElementById("menos_b");
+var igual = document.getElementById("igual_b");
+
 cero.onclick = function() {
-    barra.setAttribute("value", "0");
+    barra.setAttribute("value", barra.value+"0");
 }
 
 uno.onclick = function() {
-    barra.setAttribute("value", "1");
+    barra.setAttribute("value", barra.value+"1");
 }
 
 dos.onclick = function() {
-    barra.setAttribute("value", "2");
+    barra.setAttribute("value", barra.value+"2");
 }
 
 tres.onclick = function() {
-    barra.setAttribute("value", "3");
+    barra.setAttribute("value", barra.value+"3");
 }
 
 cuatro.onclick = function() {
-    barra.setAttribute("value", "4");
+    barra.setAttribute("value", barra.value+"4");
 }
 
 cinco.onclick = function() {
-    barra.setAttribute("value", "5");
+    barra.setAttribute("value", barra.value+"5");
 }
 
 seis.onclick = function() {
-    barra.setAttribute("value", "6");
+    barra.setAttribute("value", barra.value+"6");
 }
 
 siete.onclick = function() {
-    barra.setAttribute("value", "7");
+    barra.setAttribute("value", barra.value+"7");
 }
 
 ocho.onclick = function() {
-    barra.setAttribute("value", "8");
+    barra.setAttribute("value", barra.value+"8");
 }
 
 nueve.onclick = function() {
-    barra.setAttribute("value", "9");
+    barra.setAttribute("value", barra.value+"9");
+}
+
+mas.onclick = function() {
+    barra.setAttribute("value", barra.value+"+");
+}
+
+menos.onclick = function() {
+    barra.setAttribute("value", barra.value+"-");
+}
+
+igual.onclick = function() {
+    let cadena = barra.getAttribute("value");
+    /*EXTRACT ALL OPERANDS FROM THE STRING AND THEIR SIGN*/
+    const operandos = [];
+    let operando = "";
+    for (let idx = 0; idx < cadena.length; idx++) {
+        if (cadena[idx]=="+") {
+            operandos.push(operando);
+            operando = "";
+        } else if (cadena[idx]=="-") {
+            operandos.push(operando);
+            operando = "-";
+        } else {
+            operando += cadena[idx];
+        }
+    }
+    if (operando.length > 0) {
+        operandos.push(operando);
+    }
+
+    /*SUM ALL ELEMENTS IN THE ARRAY*/
+    suma = 0
+    for (let idx = 0; idx < operandos.length; idx++) {
+        let operando = operandos[idx];
+        if (operando.length) {
+            suma += parseFloat(operando);    
+        }
+    }
+    
+    /*alert("Resultado: "+suma);*/
+    barra.setAttribute("value", suma);
 }
